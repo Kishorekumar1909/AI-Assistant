bfb6c38ecd1f60271f55b1b75570fdb5b4120e96
Kishore kumar <kishorekumar19093@gmail.com>
2026-03-15 21:26:11 +0530

    Updated req and setting file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "whitenoise.runserver_nostatic",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
